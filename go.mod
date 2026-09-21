b082e0101ee48d479f2cbba35fe2426d2bb331ef
Kirillkriv <kirill.krivoy@icloud.com>
2026-09-22 00:03:33 +0300

    feat: pgxpool setup pkg

--- go.mod
+++ go.mod
@@ -4,4 +4,21 @@ go 1.26.0
 
 require github.com/max-messenger/max-bot-api-client-go/v2 v2.4.0
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/golang-migrate/migrate/v4 v4.20.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.11.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/rs/zerolog v1.35.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/sys v0.46.0 // indirect
+	golang.org/x/text v0.38.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
---
+++ pkg/pgpool/pgpool.go
@@ -0,0 +1,123 @@
+package pgpool
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/jackc/pgx/v5/stdlib"
+	"net"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+type Config struct {
+	PostgresHost      string        `env:"POSTGRES_HOST" env-default:"127.0.0.1"`
+	PostgresPort      int           `env:"POSTGRES_PORT" env-default:"5432"`
+	PostgresDB        string        `env:"POSTGRES_DB"   env-default:"postgres"`
+	PostgresUser      string        `env:"POSTGRES_USER" env-default:"postgres"`
+	PostgresPassword  string        `env:"POSTGRES_PASSWORD"`
+	DisableTLS        bool          `env:"POSTGRES_DISABLE_TLS"`
+	MinCons           int           `env:"POSTGRES_MIN_CONS" env-default:"5"`
+	MaxCons           int           `env:"POSTGRES_MAX_CONS" env-default:"10"`
+	MaxConnLifetime   time.Duration `env:"POSTGRES_MAX_CONN_LIFETIME"    env-default:"1h"`
+	MaxConnIdleTime   time.Duration `env:"POSTGRES_MAX_CONN_IDLE_TIME"   env-default:"30m"`
+	HealthCheckPeriod time.Duration `env:"POSTGRES_HEALTH_CHECK_PERIOD"  env-default:"1m"`
+	ConnectTimeout    time.Duration `env:"POSTGRES_CONNECT_TIMEOUT"      env-default:"5s"`
+}
+
+func NewConfig() (*Config, error) {
+	cfg := Config{}
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func (c *Config) DSN() string {
+	sslMode := "require"
+	if c.DisableTLS {
+		sslMode = "disable"
+	}
+
+	u := url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword(c.PostgresUser, c.PostgresPassword),
+		Host:   net.JoinHostPort(c.PostgresHost, strconv.Itoa(c.PostgresPort)),
+		Path:   c.PostgresDB,
+	}
+
+	q := u.Query()
+	q.Set("sslmode", sslMode)
+	u.RawQuery = q.Encode()
+
+	return u.String()
+}
+
+func NewPool(ctx context.Context, migrationsDir ...string) (*pgxpool.Pool, error) {
+	pool, err := makePool(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make pool: %w", err)
+	}
+
+	if len(migrationsDir) < 1 {
+		return pool, nil
+	}
+
+	if err := runMigrations(migrationsDir[0], pool); err != nil {
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func makePool(ctx context.Context) (*pgxpool.Pool, error) {
+	cfg, err := NewConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	poolCfg, err := pgxpool.ParseConfig(cfg.DSN())
+	if err != nil {
+		return nil, err
+	}
+
+	poolCfg.MinConns = int32(cfg.MinCons)
+	poolCfg.MaxConns = int32(cfg.MaxCons)
+	poolCfg.MaxConnLifetime = cfg.MaxConnLifetime
+	poolCfg.MaxConnIdleTime = cfg.MaxConnIdleTime
+	poolCfg.HealthCheckPeriod = cfg.HealthCheckPeriod
+
+	pool, err := pgxpool.NewWithConfig(ctx, poolCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, err
+	}
+
+	return pool, nil
+}
+
+func runMigrations(migrations string, pool *pgxpool.Pool) error {
+	source := fmt.Sprintf("file://%s", migrations)
+	db := stdlib.OpenDBFromPool(pool)
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return err
+	}
+
+	mig, err := migrate.NewWithDatabaseInstance(source, "postgresql", driver)
+	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+		return err
+	}
+
+	return mig.Up()
+}
